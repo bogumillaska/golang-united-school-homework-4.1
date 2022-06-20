0bb607bb2bbbf64a631604833616581b6259b76f
Bogumił Laska <bogumil_laska@epam.com>
2022-06-20 12:39:37 +0200

    Solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	bytes := []byte(input)
+
+	var reversedBytes []byte
+	for k, _ := range bytes {
+		reversedBytes = append(reversedBytes, bytes[len(bytes)-1-k])
+	}
+	output = string(reversedBytes)
 	return output
 }
